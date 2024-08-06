82a3912ba17b02d862c58bb19f64b7bb8a87f5ac
minhtetsoe1 <minhtetsoe186@gmail.com>
2024-08-06 21:11:16 +0630

    second commit

--- src/main/java/com/spring/service/EmailService.java
+++ src/main/java/com/spring/service/EmailService.java
@@ -1,17 +1,13 @@
 package com.spring.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class EmailService {
@@ -20,30 +16,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public static void sendOTPEmail(String toEmail, String subject, String messageBody) {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("minhtetsoe186@gmail.com", "hgna etod wjvx xhna");
-            }
-        });
-        try {
-            Message message = new MimeMessage(session);
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
-            message.setSubject(subject);
-            message.setText(messageBody);
-            Transport.send(message);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+    public void sendOTPEmail(String toEmail, String subject, String messageBody) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(messageBody);
+        javaMailSender.send(message);
     }
-
-
 
     public static class OTPDetails {
         String otp;
